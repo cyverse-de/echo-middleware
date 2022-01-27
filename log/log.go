7b3f98333abcff5ddec42513e68debb98355b21d
Sarah Roberts <sarahr@cyverse.org>
2022-01-27 14:30:40 -0700

    CORE-1670: added a function to create a new logger

--- log/log.go
+++ log/log.go
@@ -13,29 +13,34 @@ type Logger struct {
 	Entry *logrus.Entry
 }
 
+// NewLogger creates and returns a new logger.
+func NewLogger(loggerEntry *logrus.Entry) *Logger {
+	return &Logger{Entry: loggerEntry}
+}
+
 // Output returns the output writer associated with the logger.
-func (l Logger) Output() io.Writer {
+func (l *Logger) Output() io.Writer {
 	return l.Entry.Logger.Out
 }
 
 // SetOutput sets the output writer associated with the logger.
-func (l Logger) SetOutput(writer io.Writer) {
+func (l *Logger) SetOutput(writer io.Writer) {
 	l.Entry.Logger.SetOutput(writer)
 }
 
 // Prefix returns the prefix associated with the logger. This is a no-op.
-func (l Logger) Prefix() string {
+func (l *Logger) Prefix() string {
 	return ""
 }
 
 // SetPrefix sets the prefix associated with the logger. This is a no-op.
-func (l Logger) SetPrefix(_ string) {
+func (l *Logger) SetPrefix(_ string) {
 }
 
 // Level returns the level associated with the logger. The most restrictive logging level supported by
 // labstack/echo's Logger interface is ERROR. The least restrictive logging level that it supports is
 // DEBUG.
-func (l Logger) Level() gommonlog.Lvl {
+func (l *Logger) Level() gommonlog.Lvl {
 	if l.Entry.Level == logrus.PanicLevel {
 		return gommonlog.ERROR
 	} else if l.Entry.Logger.Level == logrus.FatalLevel {
@@ -54,7 +59,7 @@ func (l Logger) Level() gommonlog.Lvl {
 // SetLevel sets the level associated with the logger. The most restrictive logging level supported by
 // labstack/echo's Logger interface is ERROR. The least restrictive logging level that it supports is
 // DEBUG.
-func (l Logger) SetLevel(lvl gommonlog.Lvl) {
+func (l *Logger) SetLevel(lvl gommonlog.Lvl) {
 	if lvl == gommonlog.ERROR {
 		l.Entry.Logger.SetLevel(logrus.ErrorLevel)
 	} else if lvl == gommonlog.WARN {
@@ -67,103 +72,103 @@ func (l Logger) SetLevel(lvl gommonlog.Lvl) {
 }
 
 // SetHeader adds header fields to log messages. This is a no-op.
-func (l Logger) SetHeader(_ string) {
+func (l *Logger) SetHeader(_ string) {
 }
 
 // Print prints a log message.
-func (l Logger) Print(args ...interface{}) {
+func (l *Logger) Print(args ...interface{}) {
 	l.Entry.Print(args...)
 }
 
 // Printf prints a formatted log message.
-func (l Logger) Printf(format string, args ...interface{}) {
+func (l *Logger) Printf(format string, args ...interface{}) {
 	l.Entry.Printf(format, args...)
 }
 
 // Printj prints a JSON log message. This is currently a no-op.
-func (l Logger) Printj(_ gommonlog.JSON) {
+func (l *Logger) Printj(_ gommonlog.JSON) {
 }
 
 // Debug prints a log message at DEBUG level.
-func (l Logger) Debug(args ...interface{}) {
+func (l *Logger) Debug(args ...interface{}) {
 	l.Entry.Debug(args...)
 }
 
 // Debugf prints a formatted log message at DEBUG level.
-func (l Logger) Debugf(format string, args ...interface{}) {
+func (l *Logger) Debugf(format string, args ...interface{}) {
 	l.Entry.Debugf(format, args...)
 }
 
 // Debugj prints a JSON log message at DEBUG level. This is currently a no-op.
-func (l Logger) Debugj(_ gommonlog.JSON) {
+func (l *Logger) Debugj(_ gommonlog.JSON) {
 }
 
 // Info prints a log message at INFO level.
-func (l Logger) Info(args ...interface{}) {
+func (l *Logger) Info(args ...interface{}) {
 	l.Entry.Info(args...)
 }
 
 // Infof prints a formatted log message at INFO level.
-func (l Logger) Infof(format string, args ...interface{}) {
+func (l *Logger) Infof(format string, args ...interface{}) {
 	l.Entry.Infof(format, args...)
 }
 
 // Infoj prints a JSON log message at INFO level. This is currently a no-op.
-func (l Logger) Infoj(_ gommonlog.JSON) {
+func (l *Logger) Infoj(_ gommonlog.JSON) {
 }
 
 // Warn prints a log message at WARN level.
-func (l Logger) Warn(args ...interface{}) {
+func (l *Logger) Warn(args ...interface{}) {
 	l.Entry.Warn(args...)
 }
 
 // Warnf prints a formatted log message at WARN level.
-func (l Logger) Warnf(format string, args ...interface{}) {
+func (l *Logger) Warnf(format string, args ...interface{}) {
 	l.Entry.Warnf(format, args...)
 }
 
 // Warnj prints a JSON log satement at WARN level. This is currently a no-op.
-func (l Logger) Warnj(_ gommonlog.JSON) {
+func (l *Logger) Warnj(_ gommonlog.JSON) {
 }
 
 // Error prints a log message at ERROR level.
-func (l Logger) Error(args ...interface{}) {
+func (l *Logger) Error(args ...interface{}) {
 	l.Entry.Error(args...)
 }
 
 // Errorf prints a formatted log message at ERROR level.
-func (l Logger) Errorf(format string, args ...interface{}) {
+func (l *Logger) Errorf(format string, args ...interface{}) {
 	l.Entry.Errorf(format, args...)
 }
 
 // Errorj prints a JSON log message at ERROR level. This is currently a no-op.
-func (l Logger) Errorj(_ gommonlog.JSON) {
+func (l *Logger) Errorj(_ gommonlog.JSON) {
 }
 
 // Panic prints a log message at PANIC level and exits.
-func (l Logger) Panic(args ...interface{}) {
+func (l *Logger) Panic(args ...interface{}) {
 	l.Entry.Panic(args...)
 }
 
 // Panicf prints a formatted log message at PANIC level and exits.
-func (l Logger) Panicf(format string, args ...interface{}) {
+func (l *Logger) Panicf(format string, args ...interface{}) {
 	l.Entry.Panicf(format, args...)
 }
 
 // Panicj prints a JSON log message at PANIC level and exits. This is currently a no-op.
-func (l Logger) Panicj(_ gommonlog.JSON) {
+func (l *Logger) Panicj(_ gommonlog.JSON) {
 }
 
 // Fatal prints a log message at FATAL level and exits.
-func (l Logger) Fatal(args ...interface{}) {
+func (l *Logger) Fatal(args ...interface{}) {
 	l.Entry.Fatal(args...)
 }
 
 // Fatalf prints a formatted log message at FATAL level and exits.
-func (l Logger) Fatalf(format string, args ...interface{}) {
+func (l *Logger) Fatalf(format string, args ...interface{}) {
 	l.Entry.Fatalf(format, args...)
 }
 
 // Fatalj prints a JSON log message at FATAL level and exits. this is currently a no-op.
-func (l Logger) Fatalj(_ gommonlog.JSON) {
+func (l *Logger) Fatalj(_ gommonlog.JSON) {
 }
